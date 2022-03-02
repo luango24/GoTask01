13759d36a9da3c94018e1cda8b28adbf2ce58d3b
Luis Antonio G <luisantoniogg@hotmail.com>
2022-03-02 17:06:06 -0500

    adding the solution task files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GoTask01
-Task in Go
+Task 01 in Go
+
+## Description
+This is a response for a task of create an services that merge two existing services
+It is in Golang language and make uses of features like th standart HTTP package in Go, Handler Function, Json Parsing and string formating 
+
+## Executing program
+Clone or download the repo
+From terminal execute go run cmd/api/main.go
+Then from your favorite browser application  navigate to localhost:5000
+
+## Comments
+There are other package that can be used for API like Gin Web Framework (Gin).
+The effort to copmplete the task was 2 hours.
---
+++ cmd/api/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type User struct {
+	Name     string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
+
+type JokeResponse struct {
+	Type  string `json:"type"`
+	Value Joke   `json:"value"`
+}
+
+type Joke struct {
+	ID          string   `json:"id"`
+	CurrentJoke string   `json:"joke"`
+	Categories  []string `json:"categories"`
+}
+
+func main() {
+	http.HandleFunc("/", Task)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//Combine two existing webservices in a single response to the user
+func Task(w http.ResponseWriter, r *http.Request) {
+	response01, err := http.Get("https://names.mcquay.me/api/v0/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user User
+	byteUser, err := ioutil.ReadAll(response01.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(byteUser, &user)
+	newName := user.Name + " " + user.LastName
+
+	response02, err := http.Get("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe&limitTo=nerdy")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byteJokeResponse, err := ioutil.ReadAll(response02.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jokeResponse JokeResponse
+	json.Unmarshal(byteJokeResponse, &jokeResponse)
+
+	fmt.Fprint(w, strings.Replace(jokeResponse.Value.CurrentJoke, "John Doe", newName, -1))
+}
